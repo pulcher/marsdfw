1818b34c0c93f4f6e87d4e711c40a0bb39b6fc94
Harold Pulcher <pulcher@pulcher.biz>
2017-08-08 20:09:17 -0500

    basic stuff working

--- IoTHubGetStarted/CreateDeviceIdentity/Program.cs
+++ IoTHubGetStarted/CreateDeviceIdentity/Program.cs
@@ -10,6 +10,7 @@ namespace CreateDeviceIdentity
 {
     class Program
     {
+        // PVPGJJyvZHwJiznDD+K/TxmaXNk5npGAdpnQh86AUm8=
         static RegistryManager registryManager;
         static string connectionString = "HostName=pulcher.azure-devices.net;SharedAccessKeyName=registryReadWrite;SharedAccessKey=4upUUaMzdla7EG744vw2fe2xurCSENnI7wuorE3Ob8Y=";
 
